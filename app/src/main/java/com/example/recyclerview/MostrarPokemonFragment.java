fb54724d6ea78ae19e8da17c18b5cf17850fb4e3
Mondongo03 <rjulian@elpuig.xeill.net>
2023-12-12 19:21:35 +0100

    Me quiero matar

--- app/src/main/java/com/example/recyclerview/MostrarPokemonFragment.java
+++ app/src/main/java/com/example/recyclerview/MostrarPokemonFragment.java
@@ -1,5 +1,6 @@
 package com.example.recyclerview;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.DrawableRes;
@@ -145,13 +146,105 @@ public class MostrarPokemonFragment extends Fragment {
                         binding.tipo2.setImageResource(R.drawable.hada);
                         break;
                 }
-                binding.habilidad.setText("Habilidad "+pokemon.habilidad);
-                binding.hp.setText(String.valueOf("Hp Base: "+pokemon.hp));
-                binding.atq.setText(String.valueOf("Atq Base: "+pokemon.atq));
-                binding.def.setText(String.valueOf("Def Base: "+pokemon.def));
-                binding.sAtq.setText(String.valueOf("sAtq Base: "+pokemon.sAtq));
-                binding.sDef.setText(String.valueOf("sDef Base: "+pokemon.sDef));
-                binding.vel.setText(String.valueOf("Vel Base: "+pokemon.vel));
+                binding.habilidad.setText("Habilidad " + pokemon.habilidad);
+                binding.hp.setText(String.valueOf("Hp Base: " + pokemon.hp));
+                if (pokemon.hp < 51) {
+                    binding.hp.setTextColor(Color.rgb(250, 88, 88));
+                }
+                else if (pokemon.hp>50 && pokemon.hp<101){
+                    binding.hp.setTextColor(Color.rgb( 247, 211, 88));
+                }
+                else if (pokemon.hp>100 && pokemon.hp<151){
+                    binding.hp.setTextColor(Color.rgb( 244, 250, 88));
+                }
+                else if (pokemon.hp>150 && pokemon.hp<200){
+                    binding.hp.setTextColor(Color.rgb(130, 250, 88));
+                }
+                else if (pokemon.hp>199){
+                    binding.hp.setTextColor(Color.rgb( 88, 250, 172 ));
+                }
+                binding.atq.setText(String.valueOf("Atq Base: " + pokemon.atq));
+                if (pokemon.atq < 51) {
+                    binding.atq.setTextColor(Color.rgb(250, 88, 88));
+                }
+                else if (pokemon.atq > 50 && pokemon.atq < 101){
+                    binding.atq.setTextColor(Color.rgb(247, 211, 88));
+                }
+                else if (pokemon.atq > 100 && pokemon.atq < 151){
+                    binding.atq.setTextColor(Color.rgb(244, 250, 88));
+                }
+                else if (pokemon.atq > 150 && pokemon.atq < 200){
+                    binding.atq.setTextColor(Color.rgb(130, 250, 88));
+                }
+                else if (pokemon.atq > 199){
+                    binding.atq.setTextColor(Color.rgb(88, 250, 172));
+                }
+                binding.def.setText(String.valueOf("Def Base: " + pokemon.def));
+                if (pokemon.def < 51) {
+                    binding.def.setTextColor(Color.rgb(250, 88, 88));
+                }
+                else if (pokemon.def > 50 && pokemon.def < 101){
+                    binding.def.setTextColor(Color.rgb(247, 211, 88));
+                }
+                else if (pokemon.def > 100 && pokemon.def < 151){
+                    binding.def.setTextColor(Color.rgb(244, 250, 88));
+                }
+                else if (pokemon.def > 150 && pokemon.def < 200){
+                    binding.def.setTextColor(Color.rgb(130, 250, 88));
+                }
+                else if (pokemon.def > 199){
+                    binding.def.setTextColor(Color.rgb(88, 250, 172));
+                }
+                binding.sAtq.setText(String.valueOf("sAtq Base: " + pokemon.sAtq));
+                if (pokemon.sAtq < 51) {
+                    binding.sAtq.setTextColor(Color.rgb(250, 88, 88));
+                }
+                else if (pokemon.sAtq > 50 && pokemon.sAtq < 101){
+                    binding.sAtq.setTextColor(Color.rgb(247, 211, 88));
+                }
+                else if (pokemon.sAtq > 100 && pokemon.sAtq < 151){
+                    binding.sAtq.setTextColor(Color.rgb(244, 250, 88));
+                }
+                else if (pokemon.sAtq > 150 && pokemon.sAtq < 200){
+                    binding.sAtq.setTextColor(Color.rgb(130, 250, 88));
+                }
+                else if (pokemon.sAtq > 199){
+                    binding.sAtq.setTextColor(Color.rgb(88, 250, 172));
+                }
+                binding.sDef.setText(String.valueOf("sDef Base: " + pokemon.sDef));
+                if (pokemon.sDef < 51) {
+                    binding.sDef.setTextColor(Color.rgb(250, 88, 88));
+                }
+                else if (pokemon.sDef > 50 && pokemon.sDef < 101){
+                    binding.sDef.setTextColor(Color.rgb(247, 211, 88));
+                }
+                else if (pokemon.sDef > 100 && pokemon.sDef < 151){
+                    binding.sDef.setTextColor(Color.rgb(244, 250, 88));
+                }
+                else if (pokemon.sDef > 150 && pokemon.sDef < 200){
+                    binding.sDef.setTextColor(Color.rgb(130, 250, 88));
+                }
+                else if (pokemon.sDef > 199){
+                    binding.sDef.setTextColor(Color.rgb(88, 250, 172));
+                }
+                binding.vel.setText(String.valueOf("Vel Base: " + pokemon.vel));
+                if (pokemon.vel < 51) {
+                    binding.vel.setTextColor(Color.rgb(250, 88, 88));
+                }
+                else if (pokemon.vel > 50 && pokemon.vel < 101){
+                    binding.vel.setTextColor(Color.rgb(247, 211, 88));
+                }
+                else if (pokemon.vel > 100 && pokemon.vel < 151){
+                    binding.vel.setTextColor(Color.rgb(244, 250, 88));
+                }
+                else if (pokemon.vel > 150 && pokemon.vel < 200){
+                    binding.vel.setTextColor(Color.rgb(130, 250, 88));
+                }
+                else if (pokemon.vel > 199){
+                    binding.vel.setTextColor(Color.rgb(88, 250, 172));
+                }
+
+
             }
         });
     }
