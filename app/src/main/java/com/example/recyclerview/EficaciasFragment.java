6c8ab4cf449f360f1c418f7173560baaa46e57fb
ruben <rjulian@elpuig.xeill.net>
2023-12-13 16:39:45 +0100

    Venga va que se puede :(

---
+++ app/src/main/java/com/example/recyclerview/EficaciasFragment.java
@@ -0,0 +1,36 @@
+package com.example.recyclerview;
+
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class EficaciasFragment extends Fragment {
+
+    private EficaciasViewModel mViewModel;
+
+    public static EficaciasFragment newInstance() {
+        return new EficaciasFragment();
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_eficacias, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mViewModel = new ViewModelProvider(this).get(EficaciasViewModel.class);
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/recyclerview/EficaciasViewModel.java
@@ -0,0 +1,7 @@
+package com.example.recyclerview;
+
+import androidx.lifecycle.ViewModel;
+
+public class EficaciasViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
